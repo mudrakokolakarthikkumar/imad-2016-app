a287c1ef5bead03d0f2bf3453f258bdf12b68a60
Mudrakola Karthik Kumar <mudrakolakarthik321@gmail.com>
2016-09-25 14:24:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-1', function (req, res) {
+  res.send('this is article one ');
+});
+
+app.get('article-2', function (req, res) {
+  res.send('this is article two ');
+});
+
+app.get('article-3', function (req, res) {
+  res.send('this is article three ');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
