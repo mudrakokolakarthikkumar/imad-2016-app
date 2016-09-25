59ccef52db84308e2455e38423f499bb3eaddb90
Mudrakola Karthik Kumar <mudrakolakarthik321@gmail.com>
2016-09-25 20:00:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,5 +26,5 @@ function moveRight() {
     img.style.marginLeft= marginLeft + 'px';
 }
 img.onclick = function() {
-    var interval = setInterval(moveRight,50)
+    var interval = setInterval(moveRight,50);
 };
