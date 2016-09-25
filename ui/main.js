830fb8f9a4c71d5cb94438b4c03859562bf30083
Mudrakola Karthik Kumar <mudrakolakarthik321@gmail.com>
2016-09-25 19:42:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,11 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerText='new value';
+
+
+var img=document.getElemntById('madi');
+
+img.onclick = function() {
+    
+    img.style.marginLeft ='100px';
+}
