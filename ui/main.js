204317eb3f7ee228dd8729875f838a8ee1834ec5
Mudrakola Karthik Kumar <mudrakolakarthik321@gmail.com>
2016-11-19 09:27:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,19 +26,3 @@ button.onclick=function(){
   request.send(null);
 };
 
-var nameInput=document.getElementById('name');
-var name= nameInput.value;
-var submit =document.getElementById('submit_btn');
-submit.onclick = function() {
-    //make a request to the server and send the name
-    
-    //capture the list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0;i<names.length;i++)
-    {
-        list+='<li>' + names[i] + '</li>';
-    }
-    var ul= documentgetElementById('namelist');
-    ul.innerHTML = list;
-};
